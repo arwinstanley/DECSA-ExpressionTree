151212cf1556a90c2ccde39561bba27ba9d3c52b
Alex Winstanley <arwds2@gmail.com>
2018-03-06 10:41:10 -0500

    Update ExpressionTree.java

--- ExpressionTree/src/ExpressionTree.java
+++ ExpressionTree/src/ExpressionTree.java
@@ -1,11 +1,37 @@
 import java.util.*;
 public class ExpressionTree extends TreeNode implements Expression {
-	private Stack<Object> objStack = new Stack<Object>();
+	private Stack<TreeNode> objStack = new Stack<TreeNode>();
 	public ExpressionTree(String str) {
 		super(str);
 	}
 	public TreeNode buildTree(String[] exp) {
-		for( int i = 0 )
+		
+		for( int i = 0; i < exp.length; i++ ) {
+			TreeNode x = new TreeNode(exp[i]);
+			objStack.push(x);
+		}
+		
+		Stack<TreeNode> temp = new Stack<TreeNode>();
+		
+		while(!objStack.isEmpty()) {
+			if(isOperator(objStack.peek())) {
+				TreeNode leftTree = null;
+				TreeNode rightTree = null;
+				if(!temp.isEmpty())
+					leftTree = temp.pop();
+				if(!temp.isEmpty())
+					rightTree = temp.pop();
+				TreeNode toAdd = new TreeNode(objStack.pop(), leftTree, rightTree);
+				if(objStack.isEmpty())
+					return toAdd;
+				objStack.push(toAdd);
+			}
+			else {
+				temp.push(objStack.pop());
+			}
+		}
+		return null;
+	 
 	}
 	public int evalTree() {
 		
@@ -22,8 +48,11 @@ public class ExpressionTree extends TreeNode implements Expression {
 	public int postfixEval(String[] exp) {
 		
 	}
-	public boolean isOperator(String str) {
-		if(str.equals("+")||str.equals("*"))
+	public boolean isOperator(TreeNode tree) {
+
+		String str = (String) tree.getValue();
+		
+		if(str.equals("+")||str.equals("*")||str.equals("/")||str.equals("-")||str.equals("%"))
 			return true;
 		return false;
 	}
